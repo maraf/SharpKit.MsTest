131b61bca9014800fabd01a15174554f18c71ced
maraf <mara@neptuo.com>
2016-09-11 21:06:02 +0200

    Add project for UI.

---
+++ src/SharpKit.MsTest.UI/TestPresenter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpKit.MsTest.UI
+{
+    public class TestPresenter
+    {
+        public void Render(jQuery.jQuery root)
+        {
+        }
+    }
+}
