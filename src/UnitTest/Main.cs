24786df2cc13cdffde30eacac9aa20463133ce61
maraf <mara@neptuo.com>
2016-09-11 21:46:43 +0200

    Add example test project.

---
+++ src/UnitTest/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class Main
+    {
+        [TestMethod]
+        public void Add()
+        {
+            int a = 5;
+            int b = 7;
+            int result = a + b;
+            Assert.AreEqual(12, result);
+        }
+    }
+}
